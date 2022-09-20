0396c19eeabb91ae30792e182f4b26e9b31c86b3
sehun0219 <cloudnine0219@gmail.com>
2022-09-20 17:32:34 +1200

    html and javascript

--- PHPJS/12.php
+++ PHPJS/12.php
@@ -5,6 +5,23 @@
   <meta charset="utf-8">
 
 <body>
+<h1>JavaScript</h1>
+<script>
+  function a()
+  {
+    document.write("hello Function")
+  }
+  a();
+</script>
+
+<h1>php</h1>
+<?php
+  function a()
+  {
+    echo "hello Function";
+  }
+  a();
+ ?>
 
 </body>
 </head>
---
+++ PHPJS/13.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+
+<body>
+<h1>JavaScript</h1>
+<script>
+  function a(input)
+  {
+    return input+1;
+  }
+  document.writeln(a(1));
+  document.write(a(2));
+  document.writeln(a(3));
+</script>
+
+<h1>php</h1>
+<?php
+  function a($input)
+  {
+    return $input + 1;
+  }
+  echo a(3);
+ ?>
+
+</body>
+</head>
+
+</html>
